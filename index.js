895e35b8bd0b9e20737330eb894b4282c971895d
miisemaks <semyonpavlov@mail.ru>
2024-08-27 23:54:21 +0900

    test user profile git

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get('/hello', (req, res) => {
+    res.send('hello world')
+})
+
 app.get("/", (req, res) => {
     res.send("Hello world");
 });
